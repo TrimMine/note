6d25085e71bcca01a9ea35b3a796c47577e3ec21
cabbage <chinesebigcabbage@163.com>
2018-04-18 17:03:31 +0800

    抽象类

--- Note/English_note.c
+++ Note/English_note.c
@@ -1,3 +1,14 @@
 /*每天10个英语单词
 1.strategy  策略 战略  
 2.convert   转变 兑换 换算 侵占 
+3.decorator 装饰
+4.component 成分 组件 零件
+5.concrete  具体的 有形的 是在的 实际的
+6.hunt  捕猎
+7.feed  饲料
+8.bring 带来
+9.Compare 对比 比较
+
+
+
+
