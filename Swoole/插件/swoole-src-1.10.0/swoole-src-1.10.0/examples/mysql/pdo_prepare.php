4ceac10e4fbfb0cc6de3d74d6b02e53405931e02
cabbage <chinesebigcabbage@163.com>
2018-11-05 09:38:52 +0800

    删除多余文件

--- Swoole/插件/swoole-src-1.10.0/swoole-src-1.10.0/examples/mysql/pdo_prepare.php
+++
@@ -1,4 +0,0 @@
-<?php
-$db = new PDO("dbtype:host=127.0.0.1;dbname=test;charset=utf8", "root" ,"root");
-$query=$db->prepare("Select * from table where id=?");
-$query->excute(array($myinsecuredata));
